56b00ee42d7a1658284fa3710afd1a743a7aa793
MugenFalco <falco-ral@yandex.ru>
2026-07-04 11:18:38 +0300

    Коррекция стиля

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -1,13 +1,19 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
 import ru.yandex.practicum.filmorate.model.User;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
+
     User add(User user);
+
     User update(User user);
+
     void delete(Integer id);
+
     Optional<User> getById(Integer id);
+
     List<User> getAll();
 }
