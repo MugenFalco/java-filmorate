e722c43b7806613ced4ccafde2361b2966708ea8
damirshayk <damirshayk@yandex.ru>
2026-07-30 19:11:00 +0500

    Добавлена функциональность «Отзывы» (#9)
    * Добавлена функциональность отзывов

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorage.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.filmorate.storage.review;
+
+import ru.yandex.practicum.filmorate.model.Review;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ReviewStorage {
+
+    Review add(Review review);
+
+    Review update(Review review);
+
+    int delete(Long reviewId);
+
+    Optional<Review> getById(Long reviewId);
+
+    List<Review> getAll(Integer filmId, int count);
+
+    Optional<Boolean> getRating(Long reviewId, Integer userId);
+
+    void addRating(Long reviewId, Integer userId, boolean isLike);
+
+    void updateRating(Long reviewId, Integer userId, boolean isLike);
+
+    int deleteRating(Long reviewId, Integer userId, boolean isLike);
+
+    void changeUseful(Long reviewId, int delta);
+}
